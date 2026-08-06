8b9df253c4509100e5987120b3b4a6e762febcee
mysteryy101 <ridamemon161@gmail.com>
2026-08-06 17:39:45 +0500

    Implement password validation and loading behavior
    Added password check functionality and loading screen.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+window.onload = function () {
 
+setTimeout(function () {
+
+document.getElementById("loading").style.display = "none";
+
+document.getElementById("passwordPage").style.display = "flex";
+
+},3000);
+
+};
+
+function checkPassword(){
+
+let pass = document.getElementById("password").value;
+
+if(pass=="2024"){
+
+document.getElementById("passwordPage").style.display="none";
+
+document.getElementById("main").style.display="flex";
+
+}else{
+
+alert("Wrong Password 💔");
+
+}
+
+}
+
+function nextSection(){
+
+alert("Welcome Maha 🤍\n\nThis is only the beginning...\nOur story starts here 🌸");
+
+}
